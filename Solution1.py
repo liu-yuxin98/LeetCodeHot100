3b0bc451a4d5b4cc8478b2829e69744d8ecdbea5
yuxin <3178102653@zju.edu.cn>
2021-01-10 22:43:22 +0800

    add Solution1.py

--- Solution1.py
+++ Solution1.py
@@ -2,22 +2,13 @@
 class solution1(object):
     def twoSum(self, nums, target: int) :
         hashtable = dict()
-        for i in range(0,len(nums)):
-            if target-nums[i] in hashtable:
-                res = []
-                res.append(hashtable.get(target-nums[i]))
-                res.append(i)
-                return res
-            else:
-                hashtable[nums[i]] = i;
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
 
 
-
-s1 = solution1
-
-
-
-print(s1.twoSum(s1,[3,3],6))
 
 
 
