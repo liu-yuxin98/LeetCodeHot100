3df498177bbd149f6032f30ab58ac97a775e9dca
yuxin <3178102653@zju.edu.cn>
2021-01-11 10:17:37 +0800

    update for Solution1.java with some tests

--- Solution1.java
+++ Solution1.java
@@ -21,7 +21,7 @@ Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 * */
 
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 import java.util.*;
 
 public class Solution1 {
@@ -32,7 +32,7 @@ public class Solution1 {
         for(int i=0;i<nums.length;i++){
             /* find the answer*/
             if(hashtable.containsKey(target-nums[i])){
-                int[] res = new int[]{i,hashtable.get(target-nums[i])};
+                int[] res = new int[]{hashtable.get(target-nums[i]),i};
                 return res;
             }
             /* store key-value in map*/
@@ -43,11 +43,29 @@ public class Solution1 {
     }
 
     /* test esveral cases*/
-    public static void main(String[] args){
-        int[] nums = new int[]{3,3};
-        int target = 6;
-        int[] res = Solution1.twoSum(nums,target);
-    }
+    @Test
+    public void Testcases(){
+        /* basic case */
+        int[] nums = new int[]{2,7,11,15};
+        int target = 9;
+        int[] actual0 = Solution1.twoSum(nums,target);
+        int[] expeted0 = new int[]{0,1};
+        assertArrayEquals(actual0,expeted0);
 
+
+        /* case one  [3,3] 6  */
+        nums = new int[]{3,3};
+        target = 6;
+        int[] actual1 = Solution1.twoSum(nums,target);
+        int[] expeted1 = new int[]{0,1};
+        assertArrayEquals(actual1,expeted1);
+
+        /* case two   [3,2,4] 6*/
+        nums = new int[]{3,2,4};
+        int[] actual2 = Solution1.twoSum(nums,target);
+        int[] expeted2 = new int[]{1,2};
+        assertArrayEquals(actual2,expeted2);
+
+    }
 
 }
