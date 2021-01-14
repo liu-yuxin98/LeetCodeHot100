11362885128623a05a30f0bc33e2ac0e5ac85de3
yuxin <3178102653@zju.edu.cn>
2021-01-14 23:17:59 +0800

    update Solution3.java using moving window method

--- Solution3.java
+++ Solution3.java
@@ -6,31 +6,46 @@ import java.util.Set;
 import static org.junit.Assert.*;
 public class Solution3 {
     public int lengthOfLongestSubstring(String s) {
-        int max =0;
-        /* store chars without repeating*/
-        Set<Character> chars = new HashSet();
-        /* iterate through s*/
-        for(int i=0;i<s.length();i++) {
-            for(int j=i;j<s.length();j++){
-                /* repeat*/
-                if (chars.contains(s.charAt(j))) {
-                    /* decide if the length of the set exceeds max*/
-                    if (chars.size() > max) {
-                        max = chars.size();
-                    }
-                    /* renew set*/
-                    chars = new HashSet<>();
-                    break;
-                }
-                /* add this char to set*/
-                chars.add(s.charAt(j));
-                if(chars.size()>max){
-                    max = chars.size();
-                }
 
+        int max = 0;
+        int i =0; /* front index of window*/
+        int j = 0; /* back index of window*/
+        /* a moving window*/
+        HashSet<Character> window = new HashSet<>();
+        while(i<s.length() && j<s.length()){
+
+            while(j<s.length()){
+                /* decide if s[j] in the window -> repeat*/
+                if(window.contains(s.charAt(j))){
+                    /* record length if its > max*/
+                    if(window.size()>max){
+                        max = window.size();
+                    }
+                    /* find the index of the repeat char and set the front
+                    of the window to the first index after it
+                     */
+                    while(true){
+                        if(s.charAt(i) == s.charAt(j)){
+                            i += 1;
+                            break;
+                        }
+                        /* delete the char infront of the repeat char including the chr*/
+                        window.remove(s.charAt(i));
+                        i+=1;
+                    }
+                } else{
+                    /* add s[j] to window*/
+                    window.add(s.charAt(j));
+                    if(window.size()>max){
+                        max = window.size();
+                    }
+                }
+                j += 1;
             }
+
+
         }
-    return max;
+        return max;
     }
 @Test
 public void Testcases(){
