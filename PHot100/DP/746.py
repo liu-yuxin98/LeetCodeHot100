715d498658d21dab983e4b4f363d065e1b62a420
Alex Liu <yuxin_liu_1998@outlook.com>
2021-05-08 15:26:25 +0800

    add and mofified 746.py

--- PHot100/DP/746.py
+++ PHot100/DP/746.py
@@ -4,4 +4,19 @@ Created on Sat May  8 14:54:41 2021
 
 @author: Lenovo
 """
+def minCostClimbingStairs(cost):
+    """
+    :type cost: List[int]
+    :rtype: int
+    """
+    n = len(cost)
+    f = [0]*(n+1)
+    f[0] = 0
+    f[1] = 0
+    for i in range(2, n+1):
+        f[i] = min(f[i-2]+cost[i-2], f[i-1]+cost[i-1])
+    return f[n]
 
+
+cost = [10, 15, 20]
+print(minCostClimbingStairs(cost))
