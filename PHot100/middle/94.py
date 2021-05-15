c70dae4b37c47f32ec6a394ab6123aff075180c9
Alex Liu <yuxin_liu_1998@outlook.com>
2021-05-15 12:26:45 +0800

    add 94.py

--- PHot100/middle/94.py
+++ PHot100/middle/94.py
@@ -5,3 +5,19 @@ Created on Thu May  6 22:12:21 2021
 @author: Yuxin
 """
 
+def inorderTraversal(root):
+    """
+    :type root: TreeNode
+    :rtype: List[int]
+    """
+    nodelist = []
+    if root is None:
+        return nodelist
+    left = inorderTraversal(root.left)
+    nodelist.extend(left)
+    nodelist.append(root.val)
+    right = inorderTraversal(root.right)
+    nodelist.extend(right)
+    return nodelist
+    
+
