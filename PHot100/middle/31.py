aaefc56aa2c63291227e3de1278a5db139d67ce3
Alex Liu <yuxin_liu_1998@outlook.com>
2021-05-16 23:09:12 +0800

    add 31.py

---
+++ PHot100/middle/31.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 16 21:38:24 2021
+
+@author: yuxin
+"""
+
+
+def nextPermutation(nums):
+    """
+    :type nums: List[int]
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    i = len(nums)-2
+    while i >= 0 and nums[i] >= nums[i + 1]:
+        i -= 1
+    if i >= 0:
+        j = len(nums) - 1
+        while j >= 0 and nums[i] >= nums[j]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+
+    left, right = i + 1, len(nums) - 1
+    while left < right:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1
+    return nums
+
+nums = [1,1,5]
+print(nextPermutation(nums))
+
