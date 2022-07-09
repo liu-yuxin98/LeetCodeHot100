51b2849013772550979c881fa74113dec584db88
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2022-07-09 14:35:23 +0800

    add two_sum

--- Arrays/two_sum.py
+++ Arrays/two_sum.py
@@ -5,3 +5,10 @@ Created on Wed Jul  6 22:46:26 2022
 @author: yuxin_liu_1998
 """
 
+def two_sum(nums, target):
+    pair_dict = dict()
+    for i in range(len(nums)):
+        if target - nums[i] in  pair_dict:
+            return [pair_dict[target-nums[i]], i]
+        pair_dict[nums[i]] = i
+        
