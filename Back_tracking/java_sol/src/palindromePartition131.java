2a7c419de524dd1dd8a98f7cdc4e56bf26c7bd1f
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2022-12-08 18:41:04 +0800

    add131

--- Back_tracking/java_sol/src/palindromePartition131.java
+++ Back_tracking/java_sol/src/palindromePartition131.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class palindromePartition131 {
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class palindromePartition131 {
+    List<List<String>> res = new ArrayList<>();
+    List<String> path = new LinkedList<>();
+    public List<List<String>> partition(String s) {
+        back_track(s,0);
+        return res;
+    }
+    private void back_track(String s, int start){
+        if(start>=s.length()){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i=start;i<s.length(); i++){
+            String left = s.substring(start,i+1);
+            if(isPalindrome(left)){
+                path.add(left);
+            } else{
+                continue;
+            }
+            back_track(s,i+1);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    private boolean isPalindrome(String s){
+        if(s.length()==0){
+            return true;
+        }
+        int start = 0;
+        int end = s.length()-1;
+        while(true){
+            if(start>=end){
+                return true;
+            }
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start ++;
+            end --;
+        }
+    }
+
 }
