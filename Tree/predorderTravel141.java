eaf6ff9ea1df9b21e809bc3109d14dd37ec37ca4
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2022-12-16 12:04:37 +0800

    add pre post in order travel 94,144,145

---
+++ Tree/predorderTravel141.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class predorderTravel141 {
+
+    private List<Integer> IntravelHelper(TreeNode root){
+        List<Integer> path = new ArrayList<Integer>();
+        if(root == null){
+            return new ArrayList<Integer>();
+        }
+        List<Integer> left = IntravelHelper(root.left);
+        List<Integer> right = IntravelHelper(root.right);
+        path.addAll(left);
+        path.add(root.val);
+        path.addAll(right);
+        return path;
+    }
+
+    private List<Integer> posttravelHelper(TreeNode root){
+        List<Integer> path = new ArrayList<Integer>();
+        if(root == null){
+            return new ArrayList<Integer>();
+        }
+        List<Integer> left = posttravelHelper(root.left);
+        List<Integer> right = posttravelHelper(root.right);
+        path.addAll(left);
+        path.addAll(right);
+        path.add(root.val);
+        return path;
+    }
+    private List<Integer> pretravelHelper(TreeNode root){
+        List<Integer> path = new ArrayList<Integer>();
+        if(root == null){
+            return new ArrayList<Integer>();
+        }
+        path.add(root.val);
+        List<Integer> left = pretravelHelper(root.left);
+        List<Integer> right = pretravelHelper(root.right);
+        path.addAll(left);
+        path.addAll(right);
+        return path;
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> path = posttravelHelper(root);
+        return path;
+    }
+
+}
