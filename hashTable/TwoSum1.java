a7d3cd39e76356a57a4a4867c1ede97171c06008
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-01-03 00:09:37 +0800

    add twosum 1

--- hashTable/TwoSum1.java
+++ hashTable/TwoSum1.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class TwoSum1 {
+import java.util.HashMap;
+
+public class TwoSum1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])){
+                int [] res = new int[]{ map.get(nums[i]),i};
+                return res;
+            }
+            map.put(target-nums[i],i);
+        }
+        return null;
+    }
 }
