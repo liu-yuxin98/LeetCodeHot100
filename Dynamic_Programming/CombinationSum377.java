96fb75bcd170380ececfe2b5ad6c773c0b33c345
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-02-02 14:47:34 +0800

    add 377 using dp

--- Dynamic_Programming/CombinationSum377.java
+++ Dynamic_Programming/CombinationSum377.java
@@ -1,2 +1,22 @@
-package Dynamic_Programming;public class CombinationSum377 {
+package Dynamic_Programming;
+
+import java.util.Arrays;
+
+public class CombinationSum377 {
+    public int combinationSum4(int[] nums, int target) {
+        int [] dp = new int[target+1];
+        dp[0] = 1;
+        for(int i=1;i<dp.length;i++){
+            for(int j=0;j< nums.length;j++){
+                // can not fit
+                if(nums[j]>i){
+                    continue;
+                } else{
+                    // nums[j] can fit into bag
+                    dp[i] += dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
 }
