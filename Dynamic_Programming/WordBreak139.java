79636b947cf996897ef18087cd1d780efc159670
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-02-02 16:18:53 +0800

    add 139

---
+++ Dynamic_Programming/WordBreak139.java
@@ -0,0 +1,25 @@
+package Dynamic_Programming;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class WordBreak139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] valid = new boolean[s.length() + 1];
+        valid[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i && !valid[i]; j++) {
+                if (set.contains(s.substring(j, i)) && valid[j]) {
+                    valid[i] = true;
+                }
+            }
+        }
+
+        return valid[s.length()];
+
+    }
+
+}
