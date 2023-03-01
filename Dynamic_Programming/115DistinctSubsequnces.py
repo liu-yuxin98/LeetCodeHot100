6ab6f712d88d8b61f0dcbc89dced575e80cd3c27
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-03-01 18:07:13 +0800

    add 115

--- Dynamic_Programming/115DistinctSubsequnces.py
+++ Dynamic_Programming/115DistinctSubsequnces.py
@@ -49,29 +49,48 @@ class Solution:
     #     return uniqueSubsequences(0, 0)
 
     # dp
-    def numDistinct(self, s: str, t: str) -> int:
-        M, N = len(s), len(t)
-        # Dynamic Programming table
-        dp = [[0 for i in range(N + 1)] for j in range(M + 1)]
-        # Base case initialization
-        for j in range(N + 1):
-            dp[M][j] = 0
-        # Base case initialization
-        for i in range(M + 1):
-            dp[i][N] = 1
-        # Iterate over the strings in reverse so as to
-        # satisfy the way we've modeled our recursive solution
-        for i in range(M - 1, -1, -1):
-            for j in range(N - 1, -1, -1):
-                # Remember, we always need this result
-                dp[i][j] = dp[i + 1][j]
-                # If the characters match, we add the
-                # result of the next recursion call (in this
-                # case, the value of a cell in the dp table
-                if s[i] == t[j]:
-                    dp[i][j] += dp[i + 1][j + 1]
+    # def numDistinct(self, s: str, t: str) -> int:
+    #     M, N = len(s), len(t)
+    #     # Dynamic Programming table
+    #     dp = [[0 for i in range(N + 1)] for j in range(M + 1)]
+    #     # Base case initialization
+    #     for j in range(N + 1):
+    #         dp[M][j] = 0
+    #     # Base case initialization
+    #     for i in range(M + 1):
+    #         dp[i][N] = 1
+    #     # Iterate over the strings in reverse so as to
+    #     # satisfy the way we've modeled our recursive solution
+    #     for i in range(M - 1, -1, -1):
+    #         for j in range(N - 1, -1, -1):
+    #             # Remember, we always need this result
+    #             dp[i][j] = dp[i + 1][j]
+    #             # If the characters match, we add the
+    #             # result of the next recursion call (in this
+    #             # case, the value of a cell in the dp table
+    #             if s[i] == t[j]:
+    #                 dp[i][j] += dp[i + 1][j + 1]
 
-        return dp[0][0]
+    #     return dp[0][0]
+    def numDistinct(self, s: str, t: str) -> int:
+        # dp[i][j] -> num of t[0:j] in s[0:i]
+        # if t[j-1] == s[i-1] dp[i][j] = dp[i-1][j-1]+dp[i][j-1] ELSE dp[i][j] =  dp[i][j-1]
+        # initial dp[i][0] = 1,dp[0][j] = 0
+        # loop 1->mn
+        # case
+        m = len(s)
+        n = len(t)
+        dp = [[0 for j in range(n+1)] for i in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = 1
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]  # 相当于不用s[i-1]这个元素
+        print(dp)
+        return dp[m][n]
 
 
 sol = Solution()
@@ -79,12 +98,8 @@ sol = Solution()
 s = "rabbbit"
 t = "rabbit"
 
-s = "babgbag"
-t = "bag"
+# s = "babgbag"
+# t = "bag"
 
-# s = "daacaedaceacabbaabdccdaaeaebacddadcaeaacadbceaecddecdeedcebcdacdaebccdeebcbdeaccabcecbeeaadbccbaeccbbdaeadecabbbedceaddcdeabbcdaeadcddedddcececbeeabcbecaeadddeddccbdbcdcbceabcacddbbcedebbcaccac"
-# t = "ceadbaa"
-start = time.time()
+
 print(sol.numDistinct(s, t))
-end = time.time()
-print(end-start)
