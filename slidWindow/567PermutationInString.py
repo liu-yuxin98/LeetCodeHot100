bbaf249479e1ea3b8cd3019c0f2f6906397927fb
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-03-18 09:17:34 +0800

    modified 567

--- slidWindow/567PermutationInString.py
+++ slidWindow/567PermutationInString.py
@@ -1,26 +1,30 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         from collections import Counter
-        cntr, w = Counter(s1), len(s1)
-
+        counter = Counter(s1)
         for i in range(len(s2)):
-            if s2[i] in cntr:
-                cntr[s2[i]] -= 1
-            if i >= w and s2[i-w] in cntr:
-                cntr[s2[i-w]] += 1
+            if s2[i] not in counter:
+                counter = Counter(s1)
+            if i >= len(s1) and s2[i-len(s1)] in counter:
+                counter[s2[i-len(s1)]] += 1
 
-            if all([cntr[i] == 0 for i in cntr]):  # see optimized code below
-                return True
+            if s2[i] in counter:
+                counter[s2[i]] -= 1
+                for c in counter:
+                    if counter[c] != 0:
+                        break
+                else:
+                    return True
 
         return False
 
 
 s1 = "ab"
 s2 = "eidboaoo"
-s1 = "ab"
-s2 = "eidbaoo"
-# s1 = "adc"
-# s2 = "dcda"
+# s1 = "ab"
+# s2 = "eidbaoo"
+s1 = "adc"
+s2 = "dcda"
 sol = Solution()
 res = sol.checkInclusion(s1, s2)
 print(res)
