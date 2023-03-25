d98021a40744678052c1cf4947c1cf91db26e1b5
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-03-26 05:31:28 +0800

    modified quick sort

--- slidWindow/QuickSort.py
+++ slidWindow/QuickSort.py
@@ -1,41 +1,29 @@
 
 # Python3 implementation of QuickSort
 def quickSort(nums):
-    print(id(nums))
     quick_sort(nums, 0, len(nums)-1)
 
 # Function to find the partition position
 
 
 def partition(array, low, high):
-    # Choose the rightmost element as pivot
-    pivot = array[high]
-    # Pointer for greater element
-    i = low - 1
-    # Traverse through all elements
-    # compare each element with pivot
-    for j in range(low, high):
-        if array[j] <= pivot:
-            # If element smaller than pivot is found
-            # swap it with the greater element pointed by i
-            i = i + 1
-            # Swapping element at i with element at j
-            (array[i], array[j]) = (array[j], array[i])
-    # Swap the pivot element with
-    # e greater element specified by i
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-
-    # Return the position from where partition is done
-    return i + 1
+    pivot = array[low]
+    while(low < high):
+        while(low < high and array[high] >= pivot):
+            high -= 1
+        # find a array[right] < pivot
+        array[low] = array[high]
+        while(low < high and array[low] <= pivot):
+            low += 1
+        array[high] = array[low]
+    array[low] = pivot
+    return low
 
 # Function to perform quicksort
 
 
 def quick_sort(array, low, high):
     if low < high:
-        # Find pivot element such that
-        # element smaller than pivot are on the left
-        # element greater than pivot are on the right
         pi = partition(array, low, high)
         # Recursive call on the left of pivot
         quick_sort(array, low, pi - 1)
