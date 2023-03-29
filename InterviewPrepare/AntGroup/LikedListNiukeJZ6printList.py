aab1cc467adddf76f889aa25e5af9ac59e721817
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-03-29 10:27:59 +0800

    add JZ6

---
+++ InterviewPrepare/AntGroup/LikedListNiukeJZ6printList.py
@@ -0,0 +1,9 @@
+class Solution:
+    def printListFromTailToHead(self , listNode) -> list[int]:
+        # write code here
+        res = []
+        p = listNode
+        while p:
+            res.append(p.val)
+            p = p.next
+        return p
