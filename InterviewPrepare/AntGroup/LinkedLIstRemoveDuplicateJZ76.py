ac7d3736224f0de62b614ce475226561ecc35f45
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-03-30 10:32:00 +0800

    add delete duplication

--- InterviewPrepare/AntGroup/LinkedLIstRemoveDuplicateJZ76.py
+++ InterviewPrepare/AntGroup/LinkedLIstRemoveDuplicateJZ76.py
@@ -6,14 +6,34 @@ class ListNode:
 
 class Solution:
     def deleteDuplication(self, pHead):
-        sentinel = ListNode()
+        if pHead is None or pHead.next is None:
+            return pHead
+        sentinel = ListNode(None)
         sentinel.next = pHead
         cur = sentinel
-        while cur.next != None and cur.next.next != None:
+
+        while True:
+            if cur.next.next is None:
+                break
             if cur.next.val == cur.next.next.val:
-                temp = cur.next.val
-                while cur.next != None and cur.next.val == temp:
-                    cur.next = cur.next.next
+                repValue = cur.next.val
+                p = cur.next.next
+                while p and p.val == repValue:
+                    p = p.next
+                cur.next = p
             else:
                 cur = cur.next
         return sentinel.next
+
+    # def deleteDuplication(self, pHead):
+    #     sentinel = ListNode()
+    #     sentinel.next = pHead
+    #     cur = sentinel
+    #     while cur.next != None and cur.next.next != None:
+    #         if cur.next.val == cur.next.next.val:
+    #             temp = cur.next.val
+    #             while cur.next != None and cur.next.val == temp:
+    #                 cur.next = cur.next.next
+    #         else:
+    #             cur = cur.next
+    #     return sentinel.next
